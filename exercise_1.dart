dfe69191f358e462cc395cc14d4cba4c4a47844d
Aethan Ilagan <aethan.ilagan@ubx.ph>
2023-01-18 17:47:07 +0800

    Finished exercise_1

--- exercise_1.dart
+++ exercise_1.dart
@@ -3,5 +3,11 @@ import 'dart:io';
 void main() {
   print('What\'s your name?');
   String? name = stdin.readLineSync();
-  print('Hello $name!');
+  print('Hi, $name! What is your age?');
+  String? age = stdin.readLineSync()!;
+
+  int difference = 100 - int.parse(age);
+
+  print(
+      '$name, You have ${difference} ${difference == 1 ? 'year' : 'years'} to be 100');
 }
